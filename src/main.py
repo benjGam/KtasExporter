0af854d49295c2eaecf341c4a975558a841217de
Mecopi <mecopilive@gmail.com>
2023-01-24 17:59:01 +0100

    feat(env): get env variables

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
 import requests
 import json
-from dotenv import load_dotenv
+import subprocess
+from dotenv import load_dotenv, dotenv_values
+import os
 
-load_dotenv(); 
-req = requests.get('https://www.codewars.com/api/v1/users/Mecopi'); 
-resJSON = json.loads(req.content); 
+load_dotenv()
 
-print(json.dumps(resJSON, indent=4)); 
+config = dotenv_values(); 
+username = config.get('USERNAME'); 
+branch = config.get('BRANCH_TO_PUSH'); 
+local_repo_path = config.get('LOCAL_REPO_PATH'); 
+file_name = config.get('KATA_FILE_NAME'); 
+push_step = config.get('PUSH_STEP'); 
 
+def run(): 
+    req = requests.get('https://www.codewars.com/api/v1/users/Mecopi'); 
+    resJSON = json.loads(req.content); 
+    print(json.dumps(resJSON, indent=4)); 
+
+# run(); 
+
+
+# Output
+
+# bashCommand = "echo Hello World!"; 
+# process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE); 
+# output, error = process.communicate(); 

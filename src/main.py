23ed7ad28e5ae89f672dea31eb99dfda5d3e3d05
Mecopi <mecopilive@gmail.com>
2023-01-25 01:12:50 +0100

    feat(common): finish getting katas, adding getting informations methods

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import platform
 import time
 import utils
 import gvars
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -22,7 +23,7 @@ file_name = config.get('KATA_FILE_NAME');
 push_step = config.get('PUSH_STEP'); 
 
 total_completed_katas = 0; 
-completed_katas = {}; 
+completed_katas = []; 
 
 class Kata :
     def __init__(self, name, level, language, code):
@@ -30,29 +31,39 @@ class Kata :
         self.level = level; 
         self.language = language; 
         self.code = code; 
+    def __str__(self): 
+        return "Nom: " + self.name + "\nNiveau: " + self.level + '\nLanguage: ' + self.language + '\nCode: ' + self.code; 
+
 
 def connection(): 
   gvars.web_driver.find_element('id', 'user_email').send_keys(mail_address); 
   gvars.web_driver.find_element('id', 'user_password').send_keys(password); 
   utils.get_element_ByXPATH('//button[@type="submit"]').click(); 
 
-def get_katas(): 
-  solutions_divs = gvars.web_driver.find_elements(By.CLASS_NAME, "list-item-solutions"); 
-  for solution in solutions_divs: 
-    for element in solution.find_elements(By.XPATH, '*'): 
-      kata_level = get_kata_level(element); 
-      kata_name = get_kata_name(element); 
-      kata_language = ""; 
-      kata_code = ""; 
-    print(get_kata_name(element)); 
+def get_kata_code(element):
+    return BeautifulSoup(element.find_element(By.TAG_NAME, 'code').get_attribute('innerHTML'), 'html.parser').get_text(); 
 
 def get_kata_name(element): 
-  if(element.get_attribute('class') == 'item-title'):
     return element.find_element(By.TAG_NAME, 'a').text; 
 
 def get_kata_level(element): 
-  if(element.get_attribute('class') == 'item-title'): # Getting katas level
     return element.find_element(By.TAG_NAME, 'span').text;  
+
+def get_kata_language(element):
+    return element.find_element(By.TAG_NAME, 'code').get_attribute('data-language').lower(); 
+
+def get_katas(): 
+  solutions_divs = gvars.web_driver.find_elements(By.CLASS_NAME, "list-item-solutions"); 
+  for solution in solutions_divs: 
+    kata_level = get_kata_level(solution.find_element(By.CLASS_NAME, 'item-title')); 
+    kata_name = get_kata_name(solution.find_element(By.CLASS_NAME, 'item-title')); 
+    kata_language = get_kata_language(solution.find_elements(By.CLASS_NAME, 'markdown')[0]); 
+    kata_code = get_kata_code(solution.find_elements(By.CLASS_NAME, 'markdown')[0]); 
+    completed_katas.append(Kata(kata_name, kata_level, kata_language, kata_code)); 
+  
+  for kata in completed_katas:
+    print(kata); 
+
 
 def run(): 
   utils.start_browser_session(); 

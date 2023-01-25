9c09719bf1ed62605dd86953f6d73397068ce1b0
Mecopi <mecopilive@gmail.com>
2023-01-25 22:58:15 +0100

    feat(common): rework getting katas and loading pages

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import utils
 import file_management
 import gvars
 import subprocess 
+from Kata import Kata
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -19,20 +20,10 @@ load_dotenv()
 config = dotenv_values(); 
 mail_address = config.get('MAIL_ADDRESS'); 
 password = config.get('PASSWORD'); 
-branch = config.get('BRANCH_TO_PUSH'); 
 local_repo_path = config.get('LOCAL_REPO_PATH'); 
 file_name = config.get('KATA_FILE_NAME'); 
 push_step = config.get('PUSH_STEP'); 
-
-
-class Kata :
-    def __init__(self, name, level, language, code):
-        self.name = name; 
-        self.level = level; 
-        self.language = language; 
-        self.code = code; 
-    def __str__(self): 
-        return "Nom: " + self.name + "\nNiveau: " + self.level + '\nLanguage: ' + self.language + '\nCode: ' + self.code; 
+actual_getted_katas = 0; 
 
 
 def connection(): 
@@ -53,28 +44,33 @@ def get_kata_language(element):
     return element.find_element(By.TAG_NAME, 'code').get_attribute('data-language').lower(); 
 
 def load_page(): 
-    while(True): 
-        try:
-          element = gvars.web_driver.find_element(By.CLASS_NAME, 'js-infinite-marker'); 
-          gvars.web_driver.execute_script("arguments[0].scrollIntoView();", element); 
-          time.sleep(0.7);  
-        except:
-          break;  
+      try:
+        element = gvars.web_driver.find_element(By.CLASS_NAME, 'js-infinite-marker'); 
+        gvars.web_driver.execute_script("arguments[0].scrollIntoView();", element); 
+        return True; 
+      except:
+        return False; 
 
 def get_katas(): 
+  global actual_getted_katas; 
   solutions_divs = gvars.web_driver.find_elements(By.CLASS_NAME, "list-item-solutions"); 
   for solution in solutions_divs: 
-    kata_level = get_kata_level(solution.find_element(By.CLASS_NAME, 'item-title')); 
     kata_name = get_kata_name(solution.find_element(By.CLASS_NAME, 'item-title')); 
-    kata_language = get_kata_language(solution.find_elements(By.CLASS_NAME, 'markdown')[0]); 
-    kata_code = get_kata_code(solution.find_elements(By.CLASS_NAME, 'markdown')[0]); 
-    if("kyu" in kata_level):
-      gvars.completed_katas.append(Kata(kata_name, kata_level, kata_language, kata_code)); 
-  
-  gvars.completed_katas.reverse(); 
-  
+    if(actual_getted_katas >= int(push_step)): 
+      return; 
+    if (kata_name not in gvars.already_pushed_katas):
+      kata_level = get_kata_level(solution.find_element(By.CLASS_NAME, 'item-title')); 
+      kata_language = get_kata_language(solution.find_elements(By.CLASS_NAME, 'markdown')[0]); 
+      kata_code = get_kata_code(solution.find_elements(By.CLASS_NAME, 'markdown')[0]); 
+      if("kyu" in kata_level):
+        gvars.completed_katas.append(Kata(kata_name, kata_level, kata_language, kata_code)); 
+        gvars.already_pushed_katas.append(kata_name); 
+        actual_getted_katas += 1; 
+  if(load_page() == True):
+    get_katas(); 
+
 def commit():
-  bashCommand = 'cd ' + local_repo_path + ' && git add ' + file_name + ' && git commit -m "docs(common): add kata" && git push origin ' + branch; 
+  bashCommand = 'cd ' + local_repo_path + ' && git add ' + file_name + ' && git commit -m "docs(common): add kata"'; 
   os.system(bashCommand); 
 
 def run(): 
@@ -83,14 +79,14 @@ def run():
   gvars.web_driver.get("https://www.codewars.com/users/sign_in"); 
   connection(); 
   gvars.web_driver.get('https://www.codewars.com/users/Mecopi/completed_solutions'); 
-  load_page(); 
   get_katas(); 
-  i = 1; 
-
+  i = len(gvars.already_pushed_katas) - int(push_step) + 1; 
   for kata in gvars.completed_katas:
-    toWrite = "# " + kata.name + " #" + str(i) + ' [' + kata.level + ']\n' + '```js\n' + kata.code + '\n```\n'; 
+    toWrite = "# " + kata.name + " ||| #" + str(i) + ' [' + kata.level + ']\n' + '```js\n' + kata.code + '\n```\n'; 
     file_management.add_kata_in_file(local_repo_path, file_name, toWrite); 
     i += 1; 
     commit(); 
+  print('Vos ' + push_step + ' ont étés commités'); 
+
 
 run(); 

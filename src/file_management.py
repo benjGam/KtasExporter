b5aca382022639609617e2c28b89b3912dcaeb94
Mecopi <mecopilive@gmail.com>
2023-01-25 22:54:46 +0100

    feat(common): read previous added katas

--- src/file_management.py
+++ src/file_management.py
@@ -1,4 +1,6 @@
 import gvars 
+from Kata import Kata
+import traceback
 
 def add_kata_in_file(repo_path, file_name, toWrite): 
     f = open(repo_path + '/' + file_name, "a"); 
@@ -8,8 +10,11 @@ def add_kata_in_file(repo_path, file_name, toWrite):
 def read_kata_file(repo_path, file_name):
     try:
         f = open(repo_path + '/' + file_name, 'r'); 
-        content = file.read(); 
+        content = f.readlines();  
         for line in content: 
-            if()
-    except: 
-        return; 
+            if(line[0] == '#' and "kyu" in line):
+                kata_title = line[1:].split('|||')[0].strip(); 
+                gvars.already_pushed_katas.append(kata_title);  
+    except Exception as e:
+        print(e); 
+        exit; 

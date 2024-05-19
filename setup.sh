80352fc47c02b090c0b59adb52ae584af99257b9
benjGam <mecopilive@gmail.com>
2024-05-19 18:31:34 +0200

    refactor: use aliases instead of variables

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-venv_PATH="`dirname ${0}`/venv/bin/pip"
+alias pip="`dirname ${0}`/venv/bin/pip"
 
-$venv_PATH install -U selenium
-$venv_PATH install -U python-dotenv
-$venv_PATH install -U bs4
+pip install -U selenium
+pip install -U python-dotenv
+pip install -U bs4

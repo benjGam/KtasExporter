d8f451a89384a0f1992b52e74c2b5d61b92d81a4
benjGam <mecopilive@gmail.com>
2025-01-08 20:27:41 +0100

    docs: add comment to explain next steps

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ FOLDER_PATH=`dirname ${0}`
 
 python -m venv $FOLDER_PATH/venv/
 
+# Has to be improved
+# - Add other shell rc aliases
+# - Add question on adding alias
+# - Add auto source on rc file
+# - Add way to auto update alias if run file is not at defined path
+
 if [ -f "$HOME/.zshrc" ]; then
   echo "
 alias ktasexport='`pwd`/run.sh'" >> "$HOME/.zshrc"

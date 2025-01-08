874b8515993dde31c8546c46ddec9313c5a278e2
benjGam <mecopilive@gmail.com>
2025-01-08 18:58:47 +0100

    build: create 'venv' folder in 'setup.sh'

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-alias pip="`dirname ${0}`/venv/bin/pip"
+FOLDER_PATH=`dirname ${0}`
+
+python -m venv $FOLDER_PATH/venv/
+
+alias pip="$FOLDER_PATH/venv/bin/pip"
 
 pip install -U selenium
 pip install -U python-dotenv
